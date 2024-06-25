e7518fe987ec60354647205cd97f4aee8681a098
mxriuki <mwendav37@gmail.com>
2024-06-25 11:33:25 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+  customerName = "BOB";
+  return customerName;
+}
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+const leastFavoriteCustomer = "no";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bo";
+  return leastFavoriteCustomer;
+}
